57bc9d6809a5d19c8a202e5e33f2d15e97b67082
akadjsam <akadjsam@gmail.com>
2024-01-23 14:51:40 +0900

    "v0.3 day01"

--- KEB_bootcamp_cpp/day01.cpp
+++ KEB_bootcamp_cpp/day01.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main() {
     int num1;
     int num2;
     int sum;
-    cout << "input number1";
+
+    double a;
+    const double PI = 3.14;
+    cout << "input number1 : ";
     cin >> num1;
-    cout << "input number2";
+    cout << "input number2 : ";
     cin >> num2;
-    sum = num1+num2;
-    cout << "num1,num2 sum is : " << sum <<endl;
+    sum = num1 + num2;
+    cout << "num1,num2 sum is : " << sum << endl;
+
+    a = static_cast<double>(num1) / static_cast<double>(num2);
+    cout << "num1/num2 is : " << a << endl;
 }
