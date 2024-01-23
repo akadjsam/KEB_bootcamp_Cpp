944a506b8dc1746ac170b7b493330895fa61f708
akadjsam <akadjsam@gmail.com>
2024-01-23 14:02:20 +0900

    "v0.2 day01"

--- KEB_bootcamp_cpp/day01.cpp
+++ KEB_bootcamp_cpp/day01.cpp
@@ -1,17 +1,14 @@
-// #include <iostream>
-
-// using namespace std;
-// int main()
-// {
-//     cout << "만나서 반가워요" << " C++익히자"<<endl;
-//     return 0;
-// }
-
 #include <iostream>
-
 using namespace std;
-int main()
-{
-    cout << "만나서 반가워요" << " C++익히자"<<endl;
-    return 0;
+
+int main(){
+    int num1;
+    int num2;
+    int sum;
+    cout << "input number1";
+    cin >> num1;
+    cout << "input number2";
+    cin >> num2;
+    sum = num1+num2;
+    cout << "num1,num2 sum is : " << sum <<endl;
 }
