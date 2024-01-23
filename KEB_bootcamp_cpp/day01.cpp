b367458b35a5b12737c4f53543dba501755fb899
akadjsam <akadjsam@gmail.com>
2024-01-23 13:56:44 +0900

    "v0.1 day01"

--- KEB_bootcamp_cpp/day01.cpp
+++ KEB_bootcamp_cpp/day01.cpp
@@ -1,3 +1,12 @@
+// #include <iostream>
+
+// using namespace std;
+// int main()
+// {
+//     cout << "만나서 반가워요" << " C++익히자"<<endl;
+//     return 0;
+// }
+
 #include <iostream>
 
 using namespace std;
