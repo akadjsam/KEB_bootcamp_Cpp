95ae0a27fc28e8315c1f5f8052d90deb959e6854
akadjsam <akadjsam@gmail.com>
2024-01-24 10:51:27 +0900

    " v1.3 day02"

--- KEB_bootcamp_cpp/day01.cpp
+++ KEB_bootcamp_cpp/day01.cpp
@@ -1,19 +1,10 @@
 #include <iostream> //cpp는 오버,언더플로우에 대한 경고를 출력하지만 오류메세지는 출력 x
-#include <limits>
 using namespace std;
 
 int main() {
-	//uint의 최대값, 최소값
-	double num1 = numeric_limits<double>::max(); 
-	double num2 = -numeric_limits<double>::max();
+	bool a = true;
+	cout << a << endl; //true 이므로 1로 출력
+	cout << boolalpha << a << endl; //boolalpha 사용하면 boolean으로 출력
 
-	cout << num1 << endl;
-	cout << num2 << endl;
-	//값에 1000 곱하기
-	num1 *= 1000;
-	num2 *= 1000;
-
-	cout << num1 << endl; //오버플로우 발생 시 inf 무한대(숫자가 아ㅉ님)
-	cout << num2 << endl; // 언더플로우 발생 시 -inf
 	return 0;
 }
