4599853dbe951432c7cbc5595009afd2061de074
akadjsam <akadjsam@gmail.com>
2024-01-24 13:41:16 +0900

    "v1.5 day02"

--- KEB_bootcamp_cpp/day01.cpp
+++ KEB_bootcamp_cpp/day01.cpp
@@ -2,15 +2,16 @@
 using namespace std;
 
 int main() {
-	int a = 1234;
-	// if (a = 0) //a의 값이 0가 대입되어 false
-	// {
-	// 	cout << a << endl;
-	// }
-	if(a=5){ //a의 값이 5가 대입되어 true
-		cout << a << endl;
-	}
-	if(a==1234){ //a의 값이 5이므로 false
-		cout << 9999 << endl;
-	}
+    int a;
+    cout << "input : ";
+    cin >> a;
+
+    if ((a % 400 == 0) || ((a % 4 == 0) && (a % 100 != 0))) { //()기호를 잘 써주어야 한다.
+        cout << "윤년!";
+    }
+    else {
+        cout << "평년";
+    }
+
+    return 0;
 }
