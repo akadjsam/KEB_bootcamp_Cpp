05cc3ac676dd11d3b1226eb9980f38698bb26e67
akadjsam <akadjsam@gmail.com>
2024-01-24 09:37:55 +0900

    "v0.8 day02"

--- KEB_bootcamp_cpp/day01.cpp
+++ KEB_bootcamp_cpp/day01.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int main() {
 
-	cout <<"hello";
-	return 0;
+	cout << sizeof(int) << endl; //byte 단위로 return. int -> 4 byte
+	int a = 0;
+	double b = 0.12;
+	cout << sizeof(b) << endl;
 }
