1009caee76fed9a240cac8c7abc8e07c891f8bd2
akadjsam <akadjsam@gmail.com>
2024-01-24 10:45:31 +0900

    " v1.2 day02"

--- KEB_bootcamp_cpp/day01.cpp
+++ KEB_bootcamp_cpp/day01.cpp
@@ -4,16 +4,16 @@ using namespace std;
 
 int main() {
 	//uint의 최대값, 최소값
-	unsigned int num1 = numeric_limits<unsigned int>::max(); // 4294967295이 max 4294967296 변수에 대입 시 0으로 출력(오버플로우)
-	unsigned int num2 = numeric_limits<unsigned int>::min();
+	double num1 = numeric_limits<double>::max(); 
+	double num2 = -numeric_limits<double>::max();
 
 	cout << num1 << endl;
 	cout << num2 << endl;
-	//오버플로우, 언더플로우 발생
-	num1 += 1;
-	num2 -= 1;
+	//값에 1000 곱하기
+	num1 *= 1000;
+	num2 *= 1000;
 
-	cout << num1 << endl; //오버플로우 발생 시 최소값이 된다.
-	cout << num2 << endl; // 언더플로우 발생 시 최대값이 된다.
+	cout << num1 << endl; //오버플로우 발생 시 inf 무한대(숫자가 아ㅉ님)
+	cout << num2 << endl; // 언더플로우 발생 시 -inf
 	return 0;
 }
