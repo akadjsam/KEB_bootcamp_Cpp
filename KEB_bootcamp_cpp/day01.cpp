d6ef2515b89a2300ccc4c5a67b08169af5745d1a
akadjsam <akadjsam@gmail.com>
2024-01-25 14:40:42 +0900

    "v2.5 day03"

--- KEB_bootcamp_cpp/day01.cpp
+++ KEB_bootcamp_cpp/day01.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+<<<<<<< HEAD
 #include <cstdlib>
 #include <ctime>
 #include <typeinfo>
@@ -33,3 +34,41 @@ int main() {
 
 	}
 }
+=======
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	int day;
+	int week;
+	int col=1;
+
+	do {
+		cout << "한 달의 날짜 수를 입력하세요(28,29,30,31) : ";
+		cin >> day;
+	} while (day < 28 || day > 31);
+
+	do {
+		cout << "첫 날의 요일을 입력하세요(0~6) : ";
+		cin >> week;
+	} while (week < 0 || week > 6);
+
+	cout << "Sun Mon Tue Wed Thr Fri Sat" << endl;
+	cout << "--- --- --- --- --- --- ---" << endl;
+	for (int i = 0; i < week; i++) {
+		cout << "    ";
+		col++;
+	}
+	for (int i = 1; i <= day; i++) {
+		cout << setw(3) << i << " ";
+		col++;
+		if (col > 7) {
+			cout << endl;
+			col = 1;
+		}
+	}
+	return 0;
+}
+>>>>>>> parent of e0fecba (v2.4 day03)
