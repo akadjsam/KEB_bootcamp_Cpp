77be67f01d531b6fdcb8d62bc8ac63a8f9728d22
akadjsam <akadjsam@gmail.com>
2024-01-25 10:45:59 +0900

    v2.3 day03

--- KEB_bootcamp_cpp/day01.cpp
+++ KEB_bootcamp_cpp/day01.cpp
@@ -5,24 +5,33 @@ using namespace std;
 
 int main()
 {
-	int sum = 0;
-	int score = 0;
-	double avr = 0;
-	int count = 0;
-	do {
-		cout << "input : ";
-		cin >> score;
-		sum += score;
-		count++;
-		if (sum == 0) {
-			count = 2;
-			break;
-		}
-			
-	}while (score > 0);
+	int day;
+	int week;
+	int col=1;
 
-	cout << fixed << setprecision(2) << showpoint;
-	avr = static_cast<double>(sum) / count-1;
-	cout << "avr : " << avr;
+	do {
+		cout << "한 달의 날짜 수를 입력하세요(28,29,30,31) : ";
+		cin >> day;
+	} while (day < 28 || day > 31);
+
+	do {
+		cout << "첫 날의 요일을 입력하세요(0~6) : ";
+		cin >> week;
+	} while (week < 0 || week > 6);
+
+	cout << "Sun Mon Tue Wed Thr Fri Sat" << endl;
+	cout << "--- --- --- --- --- --- ---" << endl;
+	for (int i = 0; i < week; i++) {
+		cout << "    ";
+		col++;
+	}
+	for (int i = 1; i <= day; i++) {
+		cout << setw(3) << i << " ";
+		col++;
+		if (col > 7) {
+			cout << endl;
+			col = 1;
+		}
+	}
 	return 0;
 }
