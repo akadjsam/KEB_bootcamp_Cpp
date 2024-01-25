31a665220b4ec852cad1568c509d2897782d0d70
akadjsam <akadjsam@gmail.com>
2024-01-25 09:41:59 +0900

    v2.1 day03

--- KEB_bootcamp_cpp/day01.cpp
+++ KEB_bootcamp_cpp/day01.cpp
@@ -1,15 +1,31 @@
 #include <iostream>
-#include <string>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {
-	int  count = 0;
-	while(count<10){
-		cout << count << endl;
-		count ++;
-	}
+	int sum = 0;
+	int score = 0;
+	double avr = 0;
+	int count = 0;
+	while (1) {
+		cout << "input : ";
+		cin >> score;
+		if (score > 0) {
+			sum += score;
+			count++;
+		}
+		else {
+			if (sum == 0) {
+				count = 1;
+			}
+			break;
+		}
 
+	}
+	cout << fixed << setprecision(2) << showpoint;
+	avr = static_cast<double>(sum) / count;
+	cout << "avr : " << avr;
 	return 0;
 }
