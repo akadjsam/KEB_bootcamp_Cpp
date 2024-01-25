e0fecbabc5a4924a673dd3e16b683a13186fa033
akadjsam <akadjsam@gmail.com>
2024-01-25 13:47:39 +0900

    v2.4 day03

--- KEB_bootcamp_cpp/day01.cpp
+++ KEB_bootcamp_cpp/day01.cpp
@@ -1,37 +1,29 @@
 #include <iostream>
-#include <iomanip>
-
+#include <cmath>
 using namespace std;
 
-int main()
-{
-	int day;
-	int week;
-	int col=1;
+int main() {
+
+	const double PI = 3.14159265358979;
+	int n;
+	double s;
+	double peri = 0.0;
+	double area = 0.0;
+	do {
+		cout << "input n : ";
+		cin >> n;
+	} while (n < 4);
 
 	do {
-		cout << "한 달의 날짜 수를 입력하세요(28,29,30,31) : ";
-		cin >> day;
-	} while (day < 28 || day > 31);
+		cout << "input s : ";
+		cin >> s;
+	} while (s < 0.0);
+	peri = n * s;
+	area = (n * pow(s, 2)) / (4 * tan(PI / n));
 
-	do {
-		cout << "첫 날의 요일을 입력하세요(0~6) : ";
-		cin >> week;
-	} while (week < 0 || week > 6);
+	cout << peri << endl;
+	cout << area << endl;
 
-	cout << "Sun Mon Tue Wed Thr Fri Sat" << endl;
-	cout << "--- --- --- --- --- --- ---" << endl;
-	for (int i = 0; i < week; i++) {
-		cout << "    ";
-		col++;
-	}
-	for (int i = 1; i <= day; i++) {
-		cout << setw(3) << i << " ";
-		col++;
-		if (col > 7) {
-			cout << endl;
-			col = 1;
-		}
-	}
 	return 0;
+
 }
