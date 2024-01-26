bbc5e665afdc01298259b12d2afa4aec9c89f340
akadjsam <akadjsam@gmail.com>
2024-01-26 10:18:44 +0900

    v3.0 day04

--- KEB_bootcamp_cpp/day01.cpp
+++ KEB_bootcamp_cpp/day01.cpp
@@ -1,158 +1,39 @@
-// #include <iostream>
-// #include <cstdlib>
-// #include <ctime>
-// #include <typeinfo>
-// using namespace std;
-
-// int main() {
-// 	double sum = 0.0;
-// 	double product = 1.0;
-// 	double num;
-// 	int count = 0;
-// 	int size = 0;
-// 	do {
-// 		cout << "input size : ";
-// 		cin >> size;
-// 	} while (size < 0);
-
-// 	while (count < size) {
-// 		cout << "input : ";
-// 		cin >> num;
-// 		sum += num;
-// 		product *= num;
-// 		count++;
-// 	}
-// 	cout << sum << ", " << product;
-// 	return 0;
-// }
-
-// #include <iostream>
-// #include <cstdlib>
-// #include <ctime>
-// #include <typeinfo>
-// using namespace std;
-
-// int fac(int n) {
-// 	if (n == 1) {
-// 		return n;
-// 	}
-// 	else {
-// 		return n * fac(n - 1);
-// 	}
-// }
-
-// int main() {
-// 	int num;
-// 	do {
-// 		cout << "input : ";
-// 		cin >> num;
-// 	} while (num < 1);
-// 	cout << fac(num);
-// 	return 0;
-// }
-
-// #include <iostream>
-// #include <cstdlib>
-// #include <ctime>
-// #include <typeinfo>
-// using namespace std;
-
-// int user_pow(int x, int y) {
-// 	int result = 1;
-// 	for (int i = 0; i < y; i++) {
-// 		result *= x;
-// 	}
-// 	return result;
-// }
-
-// int main() {
-// 	int num1;
-// 	int num2;
-// 	do {
-// 		cout << "input n1 : ";
-// 		cin >> num1;
-// 		cout << "input n2 : ";
-// 		cin >> num2;
-// 	} while (num1 < 2 || num2 < 1);
-// 	cout << user_pow(num1, num2);
-// 	return 0;
-// }
-
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <typeinfo>
-#include <limits>
 
 using namespace std;
 
-int intvalue() {
-	int number;
-	while (1) {
-		cout << "input number : ";
-		if (cin >> number) {
-			return number;
-		}
-		else {
-			return 0;
-		}
-	}
+class Circle {
+	private:
+		double radius; //private은 오직 자기 자신의 클래스 내에서만 접근 가능, protected는 상속받은 클래스도 접근 가능
+	public:
+		double getRadius() const;
+		double getArea() const;
+		double getPerimeter() const;
+		void setRadius(double value);
+};
+double Circle::getRadius() const{
+	return radius;
 }
+
+double Circle::getArea() const{
+	const double PI = 3.141592;
+	return PI*radius*radius;
+}
+
+double Circle::getPerimeter() const{
+	const double PI = 3.141592;
+	return PI*2*radius;
+}
+
+void Circle::setRadius(double value){
+	radius = value;
+}
+
+
 int main() {
-	int num = 0;
-	num = intvalue();
-	for (int i = 1; i <= num; i++) {
-		if (num % i == 0) {
-			cout << i << endl;
-		}
-	}
+	Circle c;
+	c.setRadius(10.0);
+	cout << c.getPerimeter();
+
+	return 0;
 }
-
-// #include <iostream>
-// #include <cstdlib>
-// #include <ctime>
-// #include <typeinfo>
-// #include <limits>
-
-// using namespace std;
-
-// double checking() {
-// 	double a=0;
-// 	while (1) {
-		
-// 		cout << "input score! : ";
-// 		if (cin >> a) {
-// 			return a;
-// 		}
-// 		else {
-// 			return 0;
-// 		}
-// 	}
-// }
-
-// char result(int n) {
-// 	char grade;
-// 	if (n >= 90) {
-// 		grade = 'A';
-// 	}
-// 	else if (n >= 80) {
-// 		grade = 'B';
-// 	}
-// 	else if (n >= 70) {
-// 		grade = 'C';
-// 	}
-// 	else if (n >= 60) {
-// 		grade = 'D';
-// 	}
-// 	else {
-// 		grade = 'F';
-// 	}
-// 	return grade;
-// }
-
-// int main() {
-// 	double score;
-// 	score = checking();
-// 	cout << result(score);
-// 	return 0;
-// }
