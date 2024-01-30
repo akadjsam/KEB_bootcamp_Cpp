e28e2948ddfd2ba758572af5e81aea662424ba64
akadjsam <akadjsam@gmail.com>
2024-01-30 16:34:15 +0900

    v5.2 day06

--- KEB_bootcamp_cpp/main.cpp
+++ KEB_bootcamp_cpp/main.cpp
@@ -2,18 +2,23 @@
 #include <iomanip>
 using namespace std;
 
-void print(int(*pArr)[4], int row);
+int main(){
+    int size;
+    int* pArr;
 
-int main() {
-    int arr[3][4] = { {1,2,3,4},{5,6,7,8},{9,10,11,12} };
-    print(arr, 3);
-
-}
-void print(int(*pArr)[4], int row) {
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < 4; j++) {
-            cout << pArr[i][j] << " ";
-        }
-        cout << endl;
+    do{
+        cout << "input : ";
+        cin >> size;
+    }while(size<=0);
+    pArr = new int[size];
+    for(int i=0;i<size;i++){
+        cout << "input value : ";
+        cin >> *(pArr+i);
     }
+
+    for(int i=0;i<size;i++){
+        cout << *(pArr + i) << ' ';
+    }
+    delete[] pArr;
+    return 0;
 }
